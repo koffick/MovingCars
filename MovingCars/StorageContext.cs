5f4b4067eb2471ef05e7b1bc46b93d35d33e8808
Секисов Константин Витальевич <ksekisov@tmn.vostok-electra.ru>
2017-10-23 09:09:08 +0500

    first commit

---
+++ MovingCars/StorageContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovingCars
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext()
+            : base("name=DefaultConnection")
+        { }
+    }
+}
