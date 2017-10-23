e9f27d081ce3cf026af042cf717e7431c0d2c91e
koffick <koffick@mail.ru>
2017-10-23 14:24:42 +0500

    Добавил jquery.datables сделал представление для списка водителей с помощью этого плагина.

---
+++ MovingCars/Controllers/DriverController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovingCars.Controllers
+{
+    public class DriverController : Controller
+    {
+        StorageContext db = new StorageContext();
+
+        // GET: Driver
+        public ActionResult List()
+        {
+            ViewBag.Message = "Список водителей, с возможностью редактирования.";
+            var drivers = db.Drivers;
+            return View(drivers.ToList());
+        }
+    }
+}
