f1432b9d9e00e2116b9c60d32b83ae6d001a128e
koffick <koffick@mail.ru>
2017-10-23 11:44:53 +0500

    Накропал примерный дизайн главной страницы.

--- MovingCars/Controllers/HomeController.cs
+++ MovingCars/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MovingCars.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Описание приложения.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Адрес:";
 
             return View();
         }
